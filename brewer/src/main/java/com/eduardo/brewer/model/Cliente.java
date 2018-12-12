a1a61810f74254e20422fc196f62fffe87cd3fa0
Eduardo Muller <eduardo@microsys.inf.br>
2018-12-12 17:10:33 -0200

    commit inicial

---
+++ brewer/src/main/java/com/eduardo/brewer/model/Cliente.java
@@ -0,0 +1,27 @@
+package com.eduardo.brewer.model;
+
+public class Cliente {
+	
+	private String nome;
+	
+	private String tipoPessoa;
+	
+	private String cpfCnpj;
+	
+	private String telefone;
+	
+	private String email;
+	
+	private String logradouro;
+	
+	private String numero;
+	
+	private String complemento;
+	
+	private String cep;
+	
+	private String estado;
+	
+	private String cidade;	
+
+}
